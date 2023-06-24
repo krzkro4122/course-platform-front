741d2cf46ddbe6903da082afc7c9909c16711bd9
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-06-24 23:08:09 +0200

    commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthProvider, { AuthContext } from "./Authentication/AuthProvider";
+
+import AuthProvider from "./Authentication/AuthProvider";
 import CourseBrowser from "./Course/CourseBrowser";
 import LeagueBrowser from "./League/LeagueBrowser";
 import TaskDashboard from "./Task/TaskDashboard";
 import Register from "./Authentication/Register";
+import Scoreboard from "./Scoreboard/Scoreboard";
 import Login from "./Authentication/Login";
 import Header from "./Header";
-import Scoreboard from "./Scoreboard/Scoreboard";
 
 function App() {
   return (
